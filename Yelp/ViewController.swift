a405052a3c99cb28dc42a05d0bb3cda141404ca6
grcdeepak1 <grc.deepak@gmail.com>
2014-09-21 10:59:30 -0700

    search complete

--- Yelp/ViewController.swift
+++ Yelp/ViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     var client: YelpClient!
     var yelpbusinesses = [YelpBusiness]()
     @IBOutlet var tableView: UITableView!
+    var searchBar: UISearchBar!
     // You can register for Yelp API keys here: http://www.yelp.com/developers/manage_api_keys
     let yelpConsumerKey    = "NwlgK2WdEuPXN5-g6KYTsA"
     let yelpConsumerSecret = "EWFunrsvK2cqiJZ08E__g6ajqBM"
@@ -28,26 +29,40 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.delegate = self
         tableView.dataSource = self
         //tableView.rowHeight = UITableViewAutomaticDimension
+        
+        fetchDataForSearchTerm("Indian")
+        prepareSearchBar()
+    
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: - fetchDataForSearchTerm
+    func fetchDataForSearchTerm(term: NSString) {
+        
+        
         client = YelpClient(consumerKey: yelpConsumerKey, consumerSecret: yelpConsumerSecret, accessToken: yelpToken, accessSecret: yelpTokenSecret)
         
-        client.searchWithTerm("Thai", success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
+        client.searchWithTerm(term, success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
             //println(response)
             let json = JSON(response)
+            self.yelpbusinesses.removeAll(keepCapacity: true)
             for (i, j) in json["businesses"] {
                 self.yelpbusinesses.append(YelpBusiness(fromJson: j))
             }
             self.tableView.reloadData()
             
-        }) { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
-            println(error)
+            }) { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
+                println(error)
         }
-        prepareSearchBar()
-    
     }
     
+    // MARK: - UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return yelpbusinesses.count
     }
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCellWithIdentifier("yelpCell") as yelpCell
         let business = yelpbusinesses[indexPath.row]
@@ -55,13 +70,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return cell
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    // MARK: - prepareSearchBar
     func prepareSearchBar(){
         
-        var searchBar = UISearchBar(frame: CGRectMake(0.0, 0.0, 300, 44.0))
+        searchBar = UISearchBar(frame: CGRectMake(0.0, 0.0, 200, 44.0))
+        searchBar.delegate = self
         searchBar.autoresizingMask = UIViewAutoresizing.FlexibleRightMargin
         searchBar.backgroundColor = UIColor.clearColor()
         self.navigationItem.titleView = searchBar
@@ -72,6 +85,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         filterButton.titleLabel?.font = UIFont(name: "Helvetica Neue", size: 14.0)
         filterButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
         filterButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Highlighted)
+
         
         let leftHackItem = UIBarButtonItem(customView: filterButton)
         self.navigationItem.leftBarButtonItem = leftHackItem
@@ -79,8 +93,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         var emptyView = UIView(frame: CGRectMake(0, 0, 40.0, 44.0))
         let rightHackItem = UIBarButtonItem(customView: emptyView)
         self.navigationItem.rightBarButtonItem = rightHackItem
+    }
 
-        
+    // MARK: - UISearchBarDelegate
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        fetchDataForSearchTerm(searchBar.text as NSString)
     }
     
 }
